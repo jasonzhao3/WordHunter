341fc3c1138d90c633c823baf15abc08e296790f
Jason <yzhao3@stanford.edu>
2013-05-31 11:31:54 -0700

    problem with camera, plan to put everything in SnapWordActivity next

---
+++ WordHunter/WordHunter/src/com/ee368project/wordhunter/MainActivity.java
@@ -0,0 +1,56 @@
+package com.ee368project.wordhunter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.wordhunter.R;
+
+public class MainActivity extends Activity {
+
+	public final static String WORD_TO_SEARCH = "com.ee368project.wordhunter.MESSAGE";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		menu.add("Home");
+		menu.add("Scan Mode");
+		menu.add("Snap Mode");
+		menu.add("About");
+		return true;
+	}
+
+	/** Called when the user clicks the Scan button */
+	public void scanWord(View view) {
+		// Do something in response to button
+		// Hide the window title and set full screen
+		// Intent intent = new Intent(this, WordHunterActivity.class);
+		// EditText editText = (EditText) findViewById(R.id.edit_message);
+		// String message = editText.getText().toString();
+		// intent.putExtra(EXTRA_MESSAGE, message);
+		// startActivity(intent);
+	}
+
+	public void snapWord(View view) {
+		// Do something in response to button
+		// Hide the window title and set full screen
+		Intent intent = new Intent(this, SnapWordActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		// putExtra: first parameter: key, second parameter: value
+		intent.putExtra(WORD_TO_SEARCH, message);
+		startActivity(intent);
+
+	}
+
+}
---
+++ WordHunter/WordHunter/src/com/ee368project/wordhunter/SnapWordActivity.java
@@ -0,0 +1,57 @@
+package com.ee368project.wordhunter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.wordhunter.R;
+
+public class SnapWordActivity extends Activity {
+
+	private Preview mPreview;
+	private LabelOnTop mLabelOnTop;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// set window configuration
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+		// Get the search word from Main Activity
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.WORD_TO_SEARCH);
+		// Create our Preview view and set it as the content of our activity.
+		// Create our DrawOnTop view.
+		mLabelOnTop = new LabelOnTop(this);
+		// SnapMode: modeFlag = true
+		mPreview = new Preview(this, mLabelOnTop, 1);
+	
+		setContentView(mPreview);
+		addContentView(mLabelOnTop, new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT));
+		
+		// set the orientation as landscape
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.snap_word, menu);
+		return true;
+	}
+	
+	
+
+}
